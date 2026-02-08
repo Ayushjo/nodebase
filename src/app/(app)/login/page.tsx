9338b964add8803838bf0d7f532c6dc6c1409cc5
AyushJo <ayushs24101@iiitnr.edu.in>
2026-02-08 12:31:22 +0530

    05:Theme and Styling

--- src/app/(app)/login/page.tsx
+++ src/app/(app)/login/page.tsx
@@ -1,11 +1,14 @@
 import { LoginForm } from "@/features/auth/components/login-form";
 import { requireUnAuth } from "@/lib/auth-utils";
 
+
 const Page = async () => {
   await requireUnAuth();
   return (
     <>
-      <LoginForm />
+      
+          <LoginForm />
+    
     </>
   );
 };
